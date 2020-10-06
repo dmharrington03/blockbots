da261744b99dfb58122b06866d3ce3ae8f41f90b
Daniel Harrington <dmharrington03@gmail.com>
2020-10-06 22:40:08 +0800

    updated view button

--- view-handler.js
+++ view-handler.js
@@ -1,13 +1,15 @@
-function toggle_view() {
+function code_view() {
     let blockFrame = document.querySelector('#blocks');
     let codeFrame = document.querySelector('#code');
 
-    if (blockFrame.style.visibility === "hidden") {
-        blockFrame.style.visibility = "visible";
-        codeFrame.style.visibility = "hidden";
-    } else {
-        codeFrame.style.visibility = "visible";
-        blockFrame.style.visibility = "hidden";
-    }
+    blockFrame.style.visibility = "hidden";
+    codeFrame.style.visibility = "visible";
+}
 
+function block_view() {
+    let blockFrame = document.querySelector('#blocks');
+    let codeFrame = document.querySelector('#code');
+
+    blockFrame.style.visibility = "visible";
+    codeFrame.style.visibility = "hidden";
 }
